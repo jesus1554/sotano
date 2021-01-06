59e381785b351579a1a6eeb86897793ab662b0f3
Jesús Flores <dr.techcontacto@gmail.com>
2021-01-06 14:12:07 -0600

    JUST COMPILE AAAAAAAAAAAAAAAAAAAAAAAAAA

--- _site/sw.js
+++ _site/sw.js
@@ -1,8 +1,8 @@
-const version = '20210106140817';
+const version = '20210106141151';
 const cacheName = `static::${version}`;
 
 const buildContentBlob = () => {
-  return ["/sotano/categories","/sotano/search","/sotano/assets/styles.css","/sotano/manifest.json","/sotano/assets/search.json","/sotano/redirects.json","/sotano/sitemap.xml","/sotano/robots.txt","/sotano/feed.xml","/sotano/assets/styles.css.map","/sotano/assets/logos/logo.jpg", "/sotano/assets/default-offline-image.png", "/sotano/assets/scripts/fetch.js"
+  return ["/sotano/categories","/sotano/search","/sotano/manifest.json","/sotano/assets/search.json","/sotano/assets/styles.css","/sotano/redirects.json","/sotano/sitemap.xml","/sotano/robots.txt","/sotano/feed.xml","/sotano/assets/logos/logo.jpg", "/sotano/assets/default-offline-image.png", "/sotano/assets/scripts/fetch.js"
   ]
 }
 
